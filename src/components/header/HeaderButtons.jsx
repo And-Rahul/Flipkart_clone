2cf98b4a2ac407c2d4e357e5473d7b2c6e729111
Andhavarapu Rahul <andhavarapurahul@gmail.com>
2022-01-26 16:34:33 +0530

    first commit

---
+++ src/components/header/HeaderButtons.jsx
@@ -0,0 +1,63 @@
+import { Box, Button, makeStyles, Typography, Badge} from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+const useStyle = makeStyles(theme => ({
+    container: {
+        display: 'flex',
+        [theme.breakpoints.down('sm')]: {
+            display: 'block'
+        }
+    },
+    wrapper: {
+        margin: '0 5% 0 auto', 
+        display: 'flex',    
+        '& > *': {
+            marginRight: 50,
+            textDecoration: 'none',
+            color: '#FFFFFF',
+            fontSize: 18,
+            alignItems: 'center',
+            [theme.breakpoints.down('sm')]: {
+                color: '#2874f0',
+                alignItems: 'center',
+                display: 'flex',
+                flexDirection: 'column',
+                marginTop: 10
+            }      
+        },
+        [theme.breakpoints.down('sm')]: {
+            display: 'block'
+        }   
+    },
+    login: {
+        color: '#2874f0',
+        background: '#FFFFFF',
+        textTransform: 'none',
+        fontWeight: 600,
+        borderRadius: 2,
+        padding: '5px 40px',
+        height: 32,
+        boxShadow: 'none',
+        [theme.breakpoints.down('sm')]: {
+            background: '#2874f0',
+            color: '#FFFFFF'
+        }   
+    }
+}));
+
+const HeaderButtons =()=>{
+    const classes = useStyle();
+    return (
+        <Box className ={classes.wrapper}>
+            <Button className={classes.login} variant="contained">Login</Button>
+            <Typography style={{ marginTop: 2 }}>More</Typography>
+            <Box className={classes.container}>
+                <Badge badgeContent={4} color="secondary">
+                    <ShoppingCart />
+                </Badge>
+                <Typography style={{ marginLeft: 10 }}>Cart</Typography>
+
+            </Box>
+        </Box>
+    )
+}
+export default HeaderButtons;
